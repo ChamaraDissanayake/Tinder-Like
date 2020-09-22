31cd769a009a91db3fce6c51b0df6eba624024ba
Chamara Dissanayake <chamara@evokes-MacBook-Pro.local>
2020-09-22 12:49:38 +0530

    testing1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ViewChildren, QueryList } from '@angular/core';
+// import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+// import { StackConfig, Stack, Card, ThrowEvent, DragEvent, SwingStackComponent, SwingCardComponent } from 'angular2-swing';
+import { StackConfig, DragEvent, SwingStackComponent, SwingCardComponent } from 'angular2-swing';
+import { of } from 'rxjs/observable/of';
+// import { isYieldExpression } from 'typescript';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,162 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('myswing1') swingStack: SwingStackComponent;
+  @ViewChildren('mycards1') swingCards: QueryList<SwingCardComponent>;
 
+  cards: Array<any>;
+  stackConfig: StackConfig;
+  recentCard: string = '';  
+  
+
+  constructor(private http: Http) {
+    this.dragImage();
+  }
+  
+  ngAfterViewInit() {
+    // Either subscribe in controller or set in HTML
+    this.swingStack.throwin.subscribe((event: DragEvent) => {
+      event.target.style.background = '#ffffff';
+    });
+    
+    this.cards = [];
+    this.addNewCards(3);
   }
 
+  // Called whenever we drag an element
+  onItemMove(element, x, y, r) {
+    var color = '';
+    var abs = Math.abs(x);
+    let min = Math.trunc(Math.min(16*16 - abs, 16*16));
+    let hexCode = this.decimalToHex(min, 2);
+    
+    if (x < 0) {
+      color = '#FF' + hexCode + hexCode;
+    } else {
+      color = '#' + hexCode + 'FF' + hexCode;
+    }
+
+    element.style.background = color;
+    element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
+
+  onItemMoveSuper(element, x, y, r) {
+    var color = '';
+    var abs = Math.abs(y);
+    let min = Math.trunc(Math.min(16*16 - abs, 16*16));
+    let hexCode = this.decimalToHex(min, 2);
+  
+    if (y < 0) {
+      color = '#' + hexCode + hexCode + 'FF';
+    } else {
+      color = '#' + 'FF' + 'FF' + hexCode;
+    }
+
+    element.style.background = color;
+    element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
+  
+  // Connected through HTML
+  voteUp(like: boolean) {
+    console.log(like, 'swipe is working');
+    // let removedCard = this.cards.shift();
+    let removedCard = this.cards.pop();
+    this.addNewCards(1);
+    if (like) {
+      this.recentCard = 'You liked: ' + removedCard.email;
+    } else {
+      this.recentCard = 'You disliked: ' + removedCard.email;
+    }
+    // console.log(this.cards,'1111')
+  }
+
+  voteSuper(superLike: boolean) {
+    console.log(superLike,'working');
+    // let removedCard = this.cards.shift();
+    let removedCard = this.cards.pop();
+    this.addNewCards(1);
+    if (superLike) {
+      this.recentCard = 'You super liked: ' + removedCard.email;
+    } else {
+      this.recentCard = 'You skipped: ' + removedCard.email;
+    }
+    // console.log(this.cards,'1111')
+  }
+  
+  // Add new cards to our array
+  addNewCards(count: number) {
+    this.http.get('https://randomuser.me/api/?results=' + count)
+    .map(data => data.json().results)
+    .subscribe(result => {
+      for (let val of result) {        
+        this.cards.push(val);
+        console.log(this.cards.length, this.cards);
+      }
+    })
+  }
+  
+  // http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
+  decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+    
+    while (hex.length < padding) {
+      hex = "0" + hex;
+    }
+    
+    return hex;
+  }
+
+  dragImage(){
+    let minThrowDistance: any;
+    let positiveOffsetX: any;
+    let positiveOffsetY: any;
+    let positiveX: any;
+    let positiveY: any;
+    
+    this.stackConfig = {
+      throwOutConfidence: (offsetX, offsetY, element) => {
+        if(offsetY<0){
+          positiveOffsetY = -1 * offsetY;
+        } else {
+          positiveOffsetY = offsetY;
+        }
+        if(offsetX<0){
+          positiveOffsetX = -1 * offsetX;
+        } else {
+          positiveOffsetX = offsetX;
+        }
+        
+        if(positiveOffsetX>=positiveOffsetY){
+          console.log('if is working');
+          minThrowDistance = Math.min(Math.abs(offsetX) / (element.offsetWidth/2), 1);
+        } else {
+          console.log('else is working');
+          minThrowDistance = Math.min(Math.abs(offsetY) / (element.offsetWidth/2), 1);
+        }
+        return minThrowDistance;
+      },
+      transform: (element, x, y, r) => {
+        if(y<0){
+          positiveY = -1 * y;
+        } else {
+          positiveY = y;
+        }
+        if(x<0){
+          positiveX = -1 * x;
+        } else {
+          positiveX = x;
+        }
+
+        if(positiveY>positiveX){
+          this.onItemMoveSuper(element, x, y, r);  
+        } else {
+          this.onItemMove(element, x, y, r);
+        }        
+      },
+      throwOutDistance: (d) => {
+        return 800;
+      }
+    };
+  }
 }
